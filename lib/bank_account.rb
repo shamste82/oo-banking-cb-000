31d8a70efce2fa1d5e44fc38af8d948838a7fbdb
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-16 00:58:30 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+  attr_accessor :balance, :status, :deposit
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+
+  def deposit(money)
+    self.balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    valid = false
+    if self.balance > 0 and self.status == 'open'
+      valid = true
+    end
+    valid
+  end
+
+  def close_account
+    self.status = 'closed'
+  end
 end
